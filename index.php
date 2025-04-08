43a45b155b92db3fef8b68eda66531ed6d8cd435
Git <naylis.lattar11@gmail.com>
2025-04-08 09:55:57 +0200

    exo2

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
-<!DOCTYPE html>
-<html>
-<body>
-
+<!-- Exo1 -->
 <?php  
 for ($x = 0; $x <= 10; $x++) {
   echo "Le nombre est: $x <br>";
 }
-?>  
+ ?> 
 
-</body>
-</html>
+<br>;
+
+<?php 
+//exo2 (1ere possibilité)
+$somme = 0;
+ 
+for ($i=0; $i <101; $i++) {
+    $somme = $somme + $i;
+    }  
+ 
+if ($i >= 101) {
+echo "La somme du calcul des nombre de 1 a 100 est de  $somme";
+}
+?>  
