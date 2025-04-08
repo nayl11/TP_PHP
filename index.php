2b3acb6d470a964174c4d8c1d99238d57d487a55
Git <naylis.lattar11@gmail.com>
2025-04-08 11:39:53 +0200

    ExercicesForeach

--- index.php
+++ index.php
@@ -3,11 +3,9 @@
 for ($x = 0; $x <= 10; $x++) {
   echo "Le nombre est: $x <br>";
 }
- ?> 
 
-<br>;
+echo "<br>";
 
-<?php 
 //exo2 (1ere possibilité)
 $somme = 0;
  
@@ -20,11 +18,8 @@ echo "La somme du calcul des nombre de 1 a 100 est de  $somme";
 }
 
 
-?>  
-<br>;
+// exo 3 
 
-<!-- exo 3 -->
-<?php
 
 for ($i = 2; $i <= 2; $i++)
 {
@@ -34,14 +29,62 @@ for ($i = 2; $i <= 2; $i++)
         echo $i.' x '.$j.' = '.$i*$j.'<br>';
     }
 }
-?>
-<br>;
-<!-- exo1 -->
-<?php  
+
+echo "<br>";
+
+// Foreach -->
+// exo1 -->
+  
 $fruits = array("Pomme", "Banane", "Orange", "Fraise", "Kiwi"); 
 
 foreach ($fruits as $x) {
   echo "$x <br>";
 }
-?> 
 
+echo "<br>";
+
+ 	// exo2 -->
+
+$personne = [
+    "nom" => "Dupont",
+    "prenom" => "Jean",
+    "age" => 30,
+    "ville" => "Paris"
+];
+foreach ($personne as $x) {
+    echo "$x <br>";
+  }
+
+  echo "<br>";
+  // exo3
+
+$etudiants = [
+    ["nom" => "Martin", "note" => 15],
+    ["nom" => "Durand", "note" => 12],
+    ["nom" => "Petit", "note" => 18]
+];
+foreach ($etudiants as $etudiant) {
+    echo $etudiant  ["nom"].$etudiant["note"]  ."<br>";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// boucle for qui décrimente de 80 a 60
+
+for ($i=80; $i >61 ; $i--) { 
+    echo "Le nombre est: $i <br>";
+}
+
+for ($i=0; $i <20;$i++) { 
+    echo "Le nombre est: $i <br>";
+}
